1b0fbce8dbd4c65dc3f458a1e245b3f7e28d2b30
Robbie Cronin <robert.cronin@uqconnect.edu.au>
2017-03-13 19:48:32 +1000

    Update templates to add website skeleton, no functionality

--- finalproject.py
+++ finalproject.py
@@ -7,49 +7,59 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Import database_setup classes
-# from database_setup import Base, Restaurant, MenuItem
+from database_setup import Base, Restaurant, MenuItem
 
 # Create session for CRUD operations
-# engine = create_engine('sqlite:///restaurantmenu.db')
-# Base.metadata.bind = engine
-# DBSession = sessionmaker(bind=engine)
-# session = DBSession()
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/restaurants')
 @app.route('/restaurants/')
 def showRestaurants():
-    # restaurants = session.query(Restaurant).all()
+    restaurants = session.query(Restaurant).all()
     return render_template('showRestaurants.html', restaurants=restaurants)
 
-@app.route('/restaurant/new')
+@app.route('/restaurants/new')
 def newRestaurant():
-    return render_template('newRestaurants.html', restaurants=restaurants)
+    return render_template('newRestaurant.html')
 
 @app.route('/restaurants/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-    return render_template('editRestaurants.html', restaurants=restaurants)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('editRestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-    return render_template('deleteRestaurants.html', restaurants=restaurants)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('deleteRestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>')
 @app.route('/restaurants/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-    return render_template('showMenu.html', restaurants=restaurants)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    menu = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return render_template('showMenu.html', restaurant=restaurant, menu=menu)
+
+@app.route('/restaurants/<int:restaurant_id>/menu/new')
+def newMenuItem(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('newMenuItem.html', restaurant=restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-    return render_template('editMenuItem.html', restaurants=restaurants)
-
-@app.route('/restaurants/<int:restaurant_id>/menu/new')
-def newMenu(restaurant_id):
-    return render_template('newMenuItem.html', restaurants=restaurants)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    menuitem = session.query(MenuItem).filter_by(id=menu_id).one()
+    return render_template('editMenuItem.html', restaurant=restaurant, menuitem=menuitem)
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
-def deleteMenuItem(restaurant_id):
-    return render_template('deleteMenuItem.html', restaurants=restaurants)
+def deleteMenuItem(restaurant_id, menu_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    menuitem = session.query(MenuItem).filter_by(id=menu_id).one()
+
+    return render_template('deleteMenuItem.html', restaurant=restaurant, menuitem=menuitem)
 
 # Setup the server
 if __name__ == '__main__':
